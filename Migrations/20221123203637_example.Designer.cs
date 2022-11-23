8be4215896865d24fd1e9fb6de4b2c5bf6cd390e
DAVID BP <finalmente.me.encontraste@outlook.com>
2022-11-23 15:40:47 -0500

    Se incorporo contexto a la migracion

--- Migrations/20221123203637_example.Designer.cs
+++ Migrations/20221123203637_example.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mikencoderx.Context;
+using AppContext = Mikencoderx.Context.AppContext;
 
 #nullable disable
 
--- Migrations/AppContextModelSnapshot.cs
+++ Migrations/AppContextModelSnapshot.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mikencoderx.Context;
+using AppContext = Mikencoderx.Context.AppContext;
 
 #nullable disable
 
