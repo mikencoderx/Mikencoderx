4430304a80acd12f3a63898b8c6909550fb77aeb
DAVID BP <finalmente.me.encontraste@outlook.com>
2022-11-24 23:31:15 -0500

    Ejemplo de vistas de tablas

--- Controllers/ProyectosController.cs
+++ Controllers/ProyectosController.cs
@@ -4,7 +4,12 @@ namespace Mikencoderx.Controllers
 {
     public class ProyectosController : Controller
     {
+        
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Crear()
         {
             return View();
         }
