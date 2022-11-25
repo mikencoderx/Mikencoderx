d7a363569ce984f3655b48ea7480714372a1903f
DAVID BP <finalmente.me.encontraste@outlook.com>
2022-11-24 23:26:32 -0500

    este es un ejemplo de menu lateral

--- Program.cs
+++ Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=LogginContraller}/{action=Index}/{id?}");
+    pattern: "{controller=Proyectos}/{action=Index}/{id?}");
 
 app.Run();
